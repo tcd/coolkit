4498455132a2f6891c2c367fb5194d5759862fd0
Clay Dunston <claydunston@gmail.com>
2019-11-19 15:13:44 -0600

    Update doc comment for `to_est

--- lib/coolkit/time.rb
+++ lib/coolkit/time.rb
@@ -3,8 +3,8 @@ require "time"
 module Coolkit
   # Convert a `UTC` {Time} object to `EST`.
   #
-  # @raise [ArgumentError] Unless passed a `UTC` formatted time.
   # @param time [Time]
+  # @raise [ArgumentError] Unless passed a `UTC` formatted Time object.
   # @return [Time]
   def self.to_est(time)
     raise ArgumentError "argument is not in UTC format" unless time.utc?()
