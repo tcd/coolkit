80cccd493ebc716816f28a3e878603fe888e48ce
Clay Dunston <claydunston@gmail.com>
2020-01-04 12:56:57 -0600

    Update errors

--- lib/coolkit/error.rb
+++
@@ -1,5 +0,0 @@
-module Coolkit
-  # Exceptions raised by Coolkit inherit from {Error}.
-  # @abstract
-  class Error < StandardError; end
-end
---
+++ lib/coolkit/errors.rb
@@ -0,0 +1,6 @@
+module Coolkit
+  module Errors
+    # Exceptions raised by Coolkit inherit from {Error}.
+    class Error < StandardError; end
+  end
+end
