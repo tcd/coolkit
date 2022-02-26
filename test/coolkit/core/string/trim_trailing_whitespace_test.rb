2c4d81b91d3989f0cff6e7441ca5c5387be1d7ef
Clay Dunston <claydunston@gmail.com>
2022-02-26 08:37:41 -0600

    Update some tests

--- test/coolkit/core/string/trim_trailing_whitespace_test.rb
+++ test/coolkit/core/string/trim_trailing_whitespace_test.rb
@@ -5,27 +5,31 @@ module CoreTest
   module StringTest
     class TrimTrailingWhitespaceTest < TestCase
 
-      def test_1
-        want = "Hello\nWorld\n"
-        have = Coolkit.trim_trailing_whitespace("Hello  \nWorld\n")
+      def test_trailing_space_in_the_middle
+        want  = "Hello\nWorld\n"
+        input = "Hello  \nWorld\n"
+        have  = Coolkit.trim_trailing_whitespace(input)
         assert_equal(want, have)
       end
 
-      def test_2
-        want = "Hello\n  World\n"
-        have = Coolkit.trim_trailing_whitespace("Hello  \n  World\n")
+      def test_trailing_whitespace_in_the_middle_and_at_the_end
+        want  = "Hello\n  World\n"
+        input = "Hello  \n  World\n"
+        have  = Coolkit.trim_trailing_whitespace(input)
         assert_equal(want, have)
       end
 
-      def test_3
-        want = "Hello\n\n  World\n"
-        have = Coolkit.trim_trailing_whitespace("Hello  \n\n  World\n  ")
+      def test_trailing_whitespace_in_the_middle_and_at_the_end_with_an_empty_line
+        want  = "Hello\n\n  World\n"
+        input = "Hello  \n\n  World\n  "
+        have  = Coolkit.trim_trailing_whitespace(input)
         assert_equal(want, have)
       end
 
-      def test_4
-        want = "Hello\n\n  World"
-        have = Coolkit.trim_trailing_whitespace("Hello  \n\n  World")
+      def test_that_empty_lines_are_unaffected
+        want  = "Hello\n\n  World"
+        input = "Hello  \n\n  World"
+        have  = Coolkit.trim_trailing_whitespace(input)
         assert_equal(want, have)
       end
 
